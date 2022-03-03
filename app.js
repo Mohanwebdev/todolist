11f9eb8716744b1c095c899e4406cac4500f7c7d
Mohanwebdev <82821485+Mohanwebdev@users.noreply.github.com>
2022-03-03 17:49:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -192,6 +192,11 @@ app.post('/customListDelete',function(req,res){
 
 
 // port 
-app.listen(3000,function(){
-    console.log("server is started at port 3000");
-})
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+ 
+app.listen(port, function() {
+  console.log("Server started succesfully");
+});   
